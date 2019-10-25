557cd3bdaa0d9415abc5e1b1845e1c24ef7afa40
Justin Phillips <justinphil02@yahoo.com>
2019-10-25 02:41:30 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,25 @@
 def starts_with_a_vowel?(word)
-
+if word.match(/^[AEIOUaeiou]/) != nil
+  return true
+else
+  return false
+end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+if text.scan(/^(un)/) != []
+  if word.scan(/\D(ing)/) != []
+return text 
+  end
+end
 end
 
 def words_five_letters_long(text)
-
+if text.match(/\d+{3,}/) != nil
+  return true
+else
+  return false
+end
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
